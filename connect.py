fe69126be8e65555a7f0c630bbb8ae5580674e64
Michael Moliterno <michael.moliterno@gmail.com>
2016-05-12 23:52:30 -0500

    more fun

--- connect.py
+++ connect.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 import pytz
 from dateutil import parser as dateparser
 import numpy as np
+import pickle
+import operator
 
 # py-trello
 from trello import TrelloClient, Board, Card
@@ -13,19 +15,7 @@ from trello import TrelloClient, Board, Card
 import dsa_config
 import trello_creds
 
-list_ids_to_names = dict((v,k) for k,v in dsa_config.SALES_BOARD_LISTS.iteritems())
-
-client = TrelloClient(**trello_creds.CREDS)
-
-
-def listCardMove_dates(card):
-    """
-        Will return the history of transitions of a card from one list to another
-        The lower the index the more resent the historical item
-
-        It returns a list of lists. The sublists are triplates of
-        starting list, ending list and when the transition occured.
-    """
+def card_move_dates(card):
     card.fetch_actions('updateCard:idList')
     res = []
     for idx in card.actions:
@@ -34,8 +24,8 @@ def listCardMove_dates(card):
         strLst = idx['data']['listBefore']['id']
         endLst = idx['data']['listAfter']['id']
         res.append([strLst, endLst, dateDate])
+    res.reverse()
     return res
-
 
 # card = client.get_card('jjU0nqPK')
 #
@@ -53,45 +43,123 @@ def listCardMove_dates(card):
 #     print (jank[0],jank[1]), jank[2], diff.days
 #     prev_move_date = move_date
 
+def get_moves_list_dict(board):
+    cards = board.open_cards()
+    moves_list_dict = defaultdict(list)
+    now = datetime.now()
 
-# this is the Trello sales pipeline, gets all the cards
-sales_board = client.get_board(dsa_config.SALES_BOARD_ID)
-cards = sales_board.open_cards()
+    for card in cards:
 
-list_moves = defaultdict(list)
+        # skip cards that are in dummy boards
+        # if card.list_id in dsa_config.SALES_LISTS_TO_EXCLUDE:
+        #     continue
 
-now = datetime.now()
+        create_date = datetime.fromtimestamp(int(card.id[:8],16))
 
-for card in cards:
+        markov_moves_list = card_move_dates(card)
 
-    # skip cards that are in dummy boards
-    if card.list_id in dsa_config.SALES_LISTS_TO_EXCLUDE:
+        print card.name
+
+        if len(markov_moves_list) == 0:
+            move = (card.list_id,None)
+            delta = now-create_date
+            moves_list_dict[move].append((card.name,delta.days))
+        else:
+            prev_move_date = create_date
+            last = len(markov_moves_list) - 1
+            for i, jank in enumerate(markov_moves_list):
+                move = (jank[0],jank[1])
+                # this is ugly and I hate timezones
+                move_date = jank[2].replace(tzinfo=None)
+                delta = move_date - prev_move_date
+                moves_list_dict[move].append((card.name,delta.days))
+
+                if i == last:
+                    move = (jank[1],None)
+                    delta = now - move_date
+                    moves_list_dict[move].append((card.name,delta.days))
+
+                prev_move_date = move_date
+
+
+    return dict(moves_list_dict)
+
+def build_file():
+    client = TrelloClient(**trello_creds.CREDS)
+    sales_board = client.get_board(dsa_config.SALES_BOARD_ID)
+    moves_list_dict = get_moves_list_dict(sales_board)
+    pickle.dump(moves_list_dict, open("moves_list_dict.p","wb"))
+    pp.pprint(moves_list_dict)
+
+#build_file()
+list_ids_to_names = dict((v,k) for k,v in dsa_config.SALES_BOARD_LISTS.iteritems())
+
+moves_list_dict = pickle.load( open( "moves_list_dict.p", "rb" ) )
+
+for key, val in moves_list_dict.iteritems():
+
+    days = [x[1] for x in val]
+
+    b = key[0]
+    e = key[1]
+
+
+    if e == None:
+        e_name = None
+
+    else:
+        try:
+            b_name = list_ids_to_names[b]
+        except KeyError as e:
+            b_name = "UNKNOWN START STATE"
+
+        try:
+            e_name = list_ids_to_names[e]
+        except KeyError as e:
+            e_name = "UNKNOWN END STATE"
+
+    mean = np.mean(days)
+    median = np.median(days)
+
+    val.sort(key=operator.itemgetter(1), reverse=True)
+    moves_list_dict[key] = [len(val),
+                            b_name,
+                            e_name,
+                            mean,
+                            median,
+                            val
+                            ]
+
+sorted_x = sorted(moves_list_dict.items(), key=operator.itemgetter(1), reverse=True)
+
+for x in sorted_x:
+    data = x[1]
+    start = data[1]
+    end = data[2]
+    count = data[0]
+
+    if end == None or count <5:
         continue
 
-    create_date = datetime.fromtimestamp(int(card.id[:8],16))
+    print "{}{} --> {} ({}){}".format("*",start,end,count,"*")
+    print "* Median Time to Move:",int(data[3])
+    print "* Mean Time to Move:",int(data[4])
+    print
+    print "Longest Moves:"
+    for move in data[5][:5]:
+        print "{} {}: {}".format("*",move[0],move[1])
+    print "Quickest Moves:"
+    bottom_five = data[5][-5:]
+    bottom_five.reverse()
+    for move in bottom_five:
+        print "{} {}: {}".format("*",move[0],move[1])
+    print
+    print
 
-    markov_moves_list = listCardMove_dates(card)
-    markov_moves_list.reverse()
-
-    if len(markov_moves_list) == 0:
-        move = (card.list_id,None)
-        delta = now-create_date
-        list_moves[move].append(delta.days)
-    else:
-        print card.name
-        prev_move_date = create_date
-        for jank in markov_moves_list:
-            move = (jank[0],jank[1])
-
-            move_date = jank[2].replace(tzinfo=None)
-            diff = move_date - prev_move_date
-
-            list_moves[move].append(diff.days)
-
-            prev_move_date = move_date
-
-key = ('541c347213f6fb8df8bde205', '541c34b9702db91fe752682f')
-print dict(list_moves)[key]
-print len(dict(list_moves)[key])
-print np.median(dict(list_moves)[key])
-print np.mean(dict(list_moves)[key])
+# key = ('541c347213f6fb8df8bde205', '541c34b9702db91fe752682f')
+# print dict(list_moves)[key]
+# print len(dict(list_moves)[key])
+#
+# listy = dict(list_moves)[key]
+# sorted_by_second = sorted(listy, key=lambda tup: tup[1], reverse=True)
+# pp.pprint(sorted_by_second)
